d5d8eb106c0ee62455876f804b66ca0bcc838df5
Rayan Wilbert <rayan.rmw@gmail.com>
2021-05-12 16:51:18 -0300

    Crate ideas finishe, with weird display

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,9 @@
-document.querySelector("button.fat").addEventListener("click", function() {
-    document.querySelector("footer").classList.toggle("hide");
-})
+
+function getModal() {
+    document.querySelector("#modal").classList.toggle("hide");
+}
+
+
+// document.querySelector("button.fat").addEventListener("click", function() {
+//     document.querySelector("footer").classList.toggle("hide");
+// })
