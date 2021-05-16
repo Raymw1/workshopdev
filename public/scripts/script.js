a24d9aa2e2d86415e46f6dd0c89484ec7eaafa3d
Rayan Wilbert <rayan.rmw@gmail.com>
2021-05-16 20:10:00 -0300

    Verify fields function

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -4,6 +4,21 @@ function getModal() {
     document.querySelector("#modal").classList.toggle("addScroll");
 }
 
+function checkFields(event) {
+    const valuesToCheck = ["title", "category", "image", "description", "link"];
+    const isEmpty = valuesToCheck.find(function(value) {
+        const checkIfIsString = typeof event.target[value].value === "string";
+        const checkEmpty = !event.target[value].value.trim();
+        if (checkIfIsString && checkEmpty) {
+            return true;
+        }
+    })
+    if (isEmpty) {
+        event.preventDefault();
+        alert("Por favor, preencha todos os campos!")
+    }
+}
+
 
 // document.querySelector("button.fat").addEventListener("click", function() {
 //     document.querySelector("footer").classList.toggle("hide");
